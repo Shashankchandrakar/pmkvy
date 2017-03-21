72e7c87a466513f350ea591ab891ffafa6c19eb4
Shashank <shashankchandrakarr@gmail.com>
2017-03-21 16:07:03 +0530

    Adding Home Page

--- app/src/main/java/com/sih/pmkvy/ui/homepage.java
+++ app/src/main/java/com/sih/pmkvy/ui/homepage.java
@@ -14,6 +14,6 @@ public class homepage extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.homepage);
     }
 }
